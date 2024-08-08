52408c1477ab757861ce746e89c3739dced51e35
Pedro Henrique Rodrigues de Barros <pedrobarros232@hotmail.com>
2024-08-08 09:29:38 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -210,7 +210,7 @@ export default function Home() {
               />
               <Project.Item
                 cover={"https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
-                title={"Rust Open API"}
+                title={"Django Authentication API"}
                 status={"In Progress"}
               />
               <Project.Item
